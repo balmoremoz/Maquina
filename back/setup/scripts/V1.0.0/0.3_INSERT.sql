1fb6b8bb5f1818136c505e3beb5041c19f540d2a
balmoremoz <balmoreavalos2@gmail.com>
2023-03-15 17:31:50 +0100

    Script insert

---
+++ back/setup/scripts/V1.0.0/0.3_INSERT.sql
@@ -0,0 +1,45 @@
+set sqlbl on;
+set define off;
+
+INSERT INTO MONEDA
+VALUES(
+    (1,20),
+    (2,20),
+    (5,20),
+    (10,20),
+    (20,20),
+    (50,20),
+    (100,20),
+    (200,20)
+
+);
+
+INSERT INTO PRODUCTO
+VALUES(
+    (1,'Lays al Punto de sal','0.49','0.8','A1','44'),
+    (2,'Lays Campesinas','0.49','0.8','A2','44'),
+    (3,'Lays Mediterraneas','0.49','0.8','A3','44'),
+    (4,'Lays gourmet','0.49','0.95','A4','45'),
+    (5,'Ruffles jamon','0.49','0.8','A5','45'),
+    (6,'Cheetos pelotazos','0.49','0.8','B1','40'),
+    (7,'Cheetos pandilla','0.49','0.8','B2','36'),
+    (8,'Conos 3d matutano','0.49','0.65','B3','40'),
+    (9,'Doritos tex mex','0.49','0.9','B4','40'),
+    (10,'NatuChips Tomate Queso y Orégano','0.38','1.1','B5','26'),
+    (11,'Choco Bom Blanco','0.59','1.5','C1','100'),
+    (12,'Galletas OREO ','0.42','1.8','C2','66'),
+    (13,'KitKat Original','0.39','1.2','C3','41.5'),
+    (14,'Mikado ChocoLeche','0.68','1.2','C4','39'),
+    (15,'Gofre con chocolate','0.62','100','C5','140'),
+    (16,'Kinder Bueno White','0.59','200','D1','39'),
+    (17,'Milka Lu','0.48','1.8','D2','35'),
+    (18,'Conguitos White','0.52','1.9','D3','45'),
+    (19,'Trident 60 minutos Menta','0.43','1.5','D4',''),
+    (20,'Trident Max Watermelon','0.43','0.8','1.6',''),
+    (21,'Bezoya','0.29','0.5','E1','500'),
+    (22,'Cocacola original','0.48','1.5','E2','330'),
+    (23,'Monster Energy Green Verde','0.96','2.2','E3','500'),
+    (24,'Fanta de Naranja','0.42','1.5','E5','330'),
+    (25,'Aquarius Original','0.48','1.7','E5','330')
+);
+set define on;
